3da22896bcd9e58c2982b7e3b1272ce032b77e29
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-02 23:34:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -304,9 +304,9 @@ elif menu == "Admin Control Center":
                     
             # --- Tambahkan ini di bagian akhir dalam blok Admin Control Center ---
             with t9:
-            st.divider()
-            st.header("💎 V-ULTRA: Enterprise Command Center")
-            st.info("Status Infrastruktur: **Dedicated Private Server (Active)**")
+                st.divider()
+                st.header("💎 V-ULTRA: Enterprise Command Center")
+                st.info("Status Infrastruktur: **Dedicated Private Server (Active)**")
             
             col_u1, col_u2 = st.columns(2)
             with col_u1:
