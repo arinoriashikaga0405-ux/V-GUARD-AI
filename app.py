ff04e66ec68299dd8430365d5903cea1683d38c1
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-05 09:47:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ else:
     st.info("Seluruh data rahasia V-GUARD kini dapat Anda akses.")
                     
     # Mendefinisikan 8 Tab agar tidak error saat dipanggil di bawah
-        t1, t2, t3, t4, t5, t6, t7, t8, t9 = st.tabs([
+    t1, t2, t3, t4, t5, t6, t7, t8, t9 = st.tabs([
         "👤 Aktivasi Klien",
         "🖥️ Ekosistem AI",
         "⚙️ Pengaturan",
