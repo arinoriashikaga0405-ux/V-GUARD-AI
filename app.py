54b755697a4e660b85f883c5f6fe747a71306a77
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-06 13:26:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -213,17 +213,17 @@ elif menu == "Admin Control Center":
                                 with st.container(border=True):
                                     st.markdown("### 👁️ YOLO / Vision AI")
                                     st.write("'Mata' digital yang memantau pergerakan visual.")  
-                        with t3:
+                            with t3:
                             st.subheader("📈 Monitoring Laba & Pencegahan Fraud")
-                        c_a, c_b = st.columns(2)
-                        with c_a:
+                            c_a, c_b = st.columns(2)
+                            with c_a:
                             st.metric("Laba Bersih", "Rp 400.250.000", delta="Normal")
-                        with c_b:
+                            with c_b:
                             st.metric("Dana Terselamatkan", "Rp 15.700.000", delta="AI Fraud Detector Aktif")
                         
-                        st.divider()
-                        st.info("🔍 **Status Deteksi Dasar:** AI sedang memantau pembatalan transaksi (Void) dan anomali input kasir harian.")
-                        st.write("🗓️ **Status Invoice H-7**: Otomatis Terjadwal untuk 12 Klien.")
+                            st.divider()
+                            st.info("🔍 **Status Deteksi Dasar:** AI sedang memantau pembatalan transaksi (Void) dan anomali input kasir harian.")
+                            st.write("🗓️ **Status Invoice H-7**: Otomatis Terjadwal untuk 12 Klien.")
             
                         with t4:
                             st.subheader("📑 Audit Dokumen Multi-Format")
