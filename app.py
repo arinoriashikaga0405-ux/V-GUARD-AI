1fd00c3e43871986058076f533d3f3ebc87e1db6
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-11 22:40:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ elif menu == "Produk & Layanan":
 <style>
 .service-card {
     background-color: #ffffff;
-    border: 1px solid #e0e0e0;
+    bborder: 1px solid #e0e0e0;
     padding: 15px;
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0,0,0,0.05);
