09e555914a96b5555ec5491069a7a61438556ddf
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-11 22:43:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,11 +68,10 @@ elif menu == "Produk & Layanan":
     st.markdown("""
 <style>
 .service-card {
-    background-color: #ffffff;
-    border: 1px solid #e0e0e0;
-    padding: 15px;
-    border-radius: 10px;
-    box-shadow: 0 4px 6px rgba(0,0,0,0.05);
+            background-color: #ffffff;
+            border: 1px solid #e0e0e0;
+            padding: 15px;
+            border-radius: 10px;
     margin-bottom: 10px;
     color: #31333F;
 }
