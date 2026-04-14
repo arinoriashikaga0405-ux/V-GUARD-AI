aa0e3811666ed36d0d0bd8cda1548c75eb6d6559
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-15 00:42:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ elif menu == "Portal Klien":
     with tab_reg:
         st.subheader("Form Order & Aktivasi Layanan")
         # --- Baris 223 & 224 tetap ---
-with tab_reg:
+    with tab_reg:
     st.subheader("Form Order & Aktivasi Layanan")
     
     # GANTI Baris 225 ke bawah dengan ini:
