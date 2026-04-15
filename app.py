7e1c7b0668921532c613296c8c505af225b4e205
arinoriashikaga0405-ux <arinoriashikaga0405@gmail.com>
2026-04-15 23:26:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ if nama_owner and nama_usaha:
             
         except Exception as e:
             st.error(f"Sistem gagal terhubung ke Google Sheets: {e}")
-    else:
+else:
         st.warning("Mohon lengkapi data pendaftaran (Nama & Usaha).")
 else:
     st.error("🚨 Mohon setujui T&C terlebih dahulu.")
